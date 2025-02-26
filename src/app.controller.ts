305ccc73f15ebb07050e8460155dce9a9aa5e5d9
unknown <wesley.bueno.silva.wbs@gmail.com>
2025-02-26 20:50:02 -0300

    feat(app.module): rota get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { PrismaService } from './database/prisma.service';
+import { randomUUID } from 'crypto';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("hello")
+  async getHello(@Body() body:any) {
+    const {name, 'function': memberFunction} = body
+    const member = await this.prisma.rocketTeamMember.create({
+      data: {
+        id: randomUUID(),
+        name,
+        function: memberFunction,
+    },
+  });
+
+    return {
+      member
+    }
   }
 }
