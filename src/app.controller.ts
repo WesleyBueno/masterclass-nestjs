ab138132e16cdb857fcc188b7b702eacda129b58
unknown <wesley.bueno.silva.wbs@gmail.com>
2025-02-27 12:51:37 -0300

    feat(create-team-member-body): body da requisição

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,13 @@ import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
 import { randomUUID } from 'crypto';
+import { CreateTeamMemberBody } from './dtos/create-team-member-body';
 @Controller()
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
   @Get("hello")
-  async getHello(@Body() body:any) {
+  async getHello(@Body() body: CreateTeamMemberBody) {
     const {name, 'function': memberFunction} = body
     const member = await this.prisma.rocketTeamMember.create({
       data: {
